3782cff10737d44c6a831a031f5ef8d5e0ce6422
Rodrigo <rodrigoms2004@gmail.com>
2019-10-07 13:35:04 -0300

    heroku support

--- src/app.py
+++ src/app.py
@@ -1,18 +1,15 @@
+import os
 from flask import Flask,render_template,request,jsonify
 from textblob import TextBlob
-import json
 
-
-with open('./src/config/config.json') as config:
-    data = json.load(config)
-    port = data['serverPort']
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
+def nao_entre_em_panico():
+    if request.headers.get('Authorization') == '42':
+        return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
+    return jsonify({"message": "Não entre em pânico!"})
 
 @app.route('/api/sentiment', methods=['POST'])
 def sentiment():
@@ -36,29 +33,10 @@ def sentiment():
 
 if __name__ == '__main__':
     # app.run()
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
 
 # python src/app.py
 
-
-# @app.route('/api/<id>', methods = ['GET', 'POST', 'DELETE', 'PUT'])
-# def api(id):
-#   if request.method == 'GET':
-#     return 'GET Method:' + id
-
-#   if request.method == 'POST':
-#     data = request.get_json()
-#     print(data)
-#     return 'JSON posted'
-
-#   if request.method == 'DELETE':
-#     return 'Deleted id:' + id
-
-#   if request.method == 'PUT':
-#     return 'Updated id:' + id
-
-#   else:
-#     return 'invalid'
-
-
+# FLASK_APP=app.py flask run
